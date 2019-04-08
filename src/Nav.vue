233eed3cd44589710c7ba3d9019804a82fdffb99
jiezai0920 <351510576@qq.com>
2019-04-08 16:09:06 +0800

    fix($header): fix img style

--- src/Nav.vue
+++ src/Nav.vue
@@ -43,7 +43,7 @@ export default {
     },
   },
   created() {
-    this.src = this.imgLogo ? this.imgLogo : logo;
+    this.src = this.imgLogo || logo;
   },
   watch: {
     loginFlg(val, oldVal) {
