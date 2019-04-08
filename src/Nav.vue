9887266c8e2d87d25bc5b002f84d5b26cd0c3e26
李梦龙 <649076408@qq.com>
2019-04-08 11:21:22 +0800

    chore($cli3): initialization

---
+++ src/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-nav">C 端 PC 版导航</div>
+</template>
+
+<script>
+export default {
+  name: 'w-nav',
+};
+</script>
+
+<style scoped lang="scss">
+@import './nav.scss';
+</style>
